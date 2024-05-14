81481709713c9b3d6e6d46e14c84f4985749c372
LucasWictor <Dahlulle@gmail.com>
2024-05-14 20:19:59 +0200

    Added HttpDelete to CourseIncludes

--- Infrastructure/Repository/CourseIncludesRepository.cs
+++ Infrastructure/Repository/CourseIncludesRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using Infrastructure.Entities;
 using Infrastructure.Contexts;
 
@@ -20,5 +21,40 @@ namespace Infrastructure.Repository
                                  .Include(ci => ci.Course)
                                  .ToListAsync();
         }
+
+        public async Task<CourseIncludesEntity> GetOneAsync(Expression<Func<CourseIncludesEntity, bool>> predicate)
+        {
+            return await _context.CourseIncludes
+                .Include(ci => ci.Course)
+                .FirstOrDefaultAsync(predicate);
+        }
+        
+        public async Task<bool> DeleteAsync(string courseId)
+        {
+            if (string.IsNullOrEmpty(courseId))
+            {
+                return false; 
+            }
+
+            try
+            {
+                var courseInclude = await _context.CourseIncludes
+                    .FirstOrDefaultAsync(ci => ci.CourseId == courseId);
+
+                if (courseInclude == null)
+                {
+                    return false; 
+                }
+
+                _context.CourseIncludes.Remove(courseInclude);
+                int result = await _context.SaveChangesAsync();
+                return result > 0; 
+            }
+            catch (Exception)
+            { 
+                return false;
+            }
+        }
+
     }
 }

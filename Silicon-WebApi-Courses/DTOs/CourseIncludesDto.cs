dcb6eb34a81d4550282e4d282c2630f3084ef395
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-15 14:02:23 +0200

    Added DTO for CourseIncludes

---
+++ Silicon-WebApi-Courses/DTOs/CourseIncludesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Silicon_WebApi_Courses.DTOs
+{
+    public class CourseIncludesDTO
+    {
+        public string CourseId { get; set; } = null!;
+        public string FACode { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}

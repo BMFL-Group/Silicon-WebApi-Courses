ad04b937175eab6728e63700395560f808092486
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-16 10:33:13 +0200

    Added programdetailsdto

--- Silicon-WebApi-Courses/DTOs/ProgramDetailsDto.cs
+++ Silicon-WebApi-Courses/DTOs/ProgramDetailsDto.cs
@@ -1,6 +1,6 @@
 ﻿public class ProgramDetailsDto
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string CourseId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }

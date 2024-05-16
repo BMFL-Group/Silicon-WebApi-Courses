2438f7ba233f3a63eceb210c850ee86e0b29c7f6
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-16 10:33:50 +0200

    Added CRUD to programdetailsrepository

--- Infrastructure/Repository/ProgramDetailsRepository.cs
+++ Infrastructure/Repository/ProgramDetailsRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Infrastructure.Contexts;
 using Infrastructure.Entities;
-
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Repository
 {
@@ -13,11 +16,78 @@ namespace Infrastructure.Repository
             _context = context;
         }
 
+        #region CREATE
         public async Task<ProgramDetailsEntity> AddProgramDetailAsync(ProgramDetailsEntity programDetail)
         {
-            _context.Set<ProgramDetailsEntity>().Add(programDetail);
-            await _context.SaveChangesAsync();
-            return programDetail;
+            _context.ProgramDetails.Add(programDetail);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return programDetail;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Database operation failed", ex);
+            }
         }
+        #endregion
+
+        #region GET
+        public async Task<ProgramDetailsEntity> GetProgramDetailAsync(string id)
+        {
+            return await _context.ProgramDetails.FindAsync(id);
+        }
+        #endregion
+
+        #region GET ALL
+        public async Task<List<ProgramDetailsEntity>> GetAllProgramDetailsAsync()
+        {
+            return await _context.ProgramDetails.ToListAsync();
+        }
+        #endregion
+
+        #region UPDATE
+        public async Task<bool> UpdateProgramDetailAsync(ProgramDetailsEntity programDetail)
+        {
+            try
+            {
+                _context.ProgramDetails.Update(programDetail);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProgramDetailExists(programDetail.Id))
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        #endregion
+
+        #region DELETE
+        public async Task<bool> DeleteProgramDetailAsync(string id)
+        {
+            var programDetail = await _context.ProgramDetails.FindAsync(id);
+            if (programDetail == null)
+            {
+                return false;
+            }
+            _context.ProgramDetails.Remove(programDetail);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region EXISTS
+        private bool ProgramDetailExists(string id)
+        {
+            return _context.ProgramDetails.Any(x => x.Id == id);
+        }
+        #endregion
     }
 }

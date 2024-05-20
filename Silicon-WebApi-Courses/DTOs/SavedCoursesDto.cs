93621bdeaea7c960bcf6eba66f7a1a9daa6c64b3
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-20 13:28:22 +0200

    Created DTO for SavedCourses

---
+++ Silicon-WebApi-Courses/DTOs/SavedCoursesDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Silicon_WebApi_Courses.DTOs
+{
+    public class SavedCoursesDto
+    {
+        public int Id { get; set; }
+        public string CourseId { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public bool IsBookmarked { get; set; }
+        public bool IsJoined { get; set; }
+    }
+}

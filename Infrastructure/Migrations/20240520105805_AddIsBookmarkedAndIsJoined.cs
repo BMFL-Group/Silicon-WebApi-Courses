af4ded9601894354e93337a2f018e115a300f4d2
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-20 13:28:33 +0200

    Updated database schema

---
+++ Infrastructure/Migrations/20240520105805_AddIsBookmarkedAndIsJoined.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddIsBookmarkedAndIsJoined : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBookmarked",
+                table: "SavedCourses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsJoined",
+                table: "SavedCourses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBookmarked",
+                table: "SavedCourses");
+
+            migrationBuilder.DropColumn(
+                name: "IsJoined",
+                table: "SavedCourses");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20240520110633_UpdateCourseIdToString.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class UpdateCourseIdToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "SavedCourses");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseId",
+                table: "SavedCourses",
+                type: "nvarchar(max)",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "SavedCourses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CourseId",
+                table: "SavedCourses",
+                type: "int",
+                nullable: false);
+        }
+    }
+}

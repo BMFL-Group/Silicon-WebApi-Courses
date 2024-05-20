e821430a5021223e91a55c316817a667270f61f1
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-20 13:27:37 +0200

    Implemented CRUD to SavedCoursesRepostiry

--- Infrastructure/Repository/SavedCoursesRepository.cs
+++ Infrastructure/Repository/SavedCoursesRepository.cs
@@ -2,6 +2,9 @@
 using Infrastructure.Entities;
 using Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
@@ -13,54 +16,62 @@ namespace Infrastructure.Repositories
         {
             _context = context;
         }
-        #region GET
+
         public async Task<List<SavedCoursesModel>> GetSavedCoursesForUserAsync(string userId)
         {
-            var savedCourses = await _context.SavedCourses
+            return await _context.SavedCourses
                 .Where(sc => sc.UserId == userId)
+                .Select(sc => new SavedCoursesModel
+                {
+                    Id = sc.Id,
+                    CourseId = sc.CourseId,
+                    UserId = sc.UserId,
+                    IsBookmarked = sc.IsBookmarked,
+                    IsJoined = sc.IsJoined
+                })
                 .ToListAsync();
-
-            var savedCourseModels = savedCourses.Select(sc => new SavedCoursesModel
-            {
-                Id = sc.Id,
-                CourseId = sc.CourseId,
-                UserId = sc.UserId
-            }).ToList();
-
-            return savedCourseModels;
         }
-        #endregion
 
-        #region CREATE
         public async Task<SavedCoursesModel> CreateSavedCourseAsync(SavedCoursesModel savedCourse)
         {
             var entity = new SavedCoursesEntity
             {
                 CourseId = savedCourse.CourseId,
-                UserId = savedCourse.UserId
+                UserId = savedCourse.UserId,
+                IsBookmarked = savedCourse.IsBookmarked,
+                IsJoined = savedCourse.IsJoined
             };
 
             _context.SavedCourses.Add(entity);
             await _context.SaveChangesAsync();
 
-            savedCourse.Id = entity.Id; 
+            savedCourse.Id = entity.Id;
             return savedCourse;
         }
-        #endregion
 
-        #region DELETE
+        public async Task<bool> UpdateSavedCourseAsync(SavedCoursesModel savedCourse)
+        {
+            var entity = await _context.SavedCourses.FindAsync(savedCourse.Id);
+            if (entity == null) return false;
+
+            entity.CourseId = savedCourse.CourseId;
+            entity.UserId = savedCourse.UserId;
+            entity.IsBookmarked = savedCourse.IsBookmarked;
+            entity.IsJoined = savedCourse.IsJoined;
+
+            _context.SavedCourses.Update(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
 
         public async Task<bool> DeleteSavedCourseAsync(int savedCourseId)
         {
             var savedCourse = await _context.SavedCourses.FindAsync(savedCourseId);
-
-            if (savedCourse == null)
-                return false;
+            if (savedCourse == null) return false;
 
             _context.SavedCourses.Remove(savedCourse);
             await _context.SaveChangesAsync();
             return true;
         }
-        #endregion
     }
 }
